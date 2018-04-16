971e8183c05ff395601bba3ad53065a61b3b727e
maciekmacku <maciejszalaj@gmail.com>
2018-04-16 23:14:38 +0200

    added buttons left and right to carousel

--- js/script.js
+++ js/script.js
@@ -1,11 +1,22 @@
 $(function () {
-	var carouselList = $('#carousel ul');
+	var carouselList = $('#carousel ul'); 
+	$('#btn-left').click("left", changeSlide); 
+	$('#btn-right').click(changeSlide);
 	setInterval(changeSlide, 5000);
-
-	function changeSlide() {
-		carouselList.animate({
-			'marginLeft': -400
-		}, 450, moveFirstSlide);
+	
+	function changeSlide(data) {
+		console.log(data);
+		if (data && data.data == "left") {
+			carouselList.animate({
+				'marginLeft': 400
+			}, 450, moveLastSlide);
+				
+		}
+		else	{
+			carouselList.animate({
+				'marginLeft': -400
+			}, 450, moveFirstSlide);
+		}
 	}
 
 	function moveFirstSlide() {
@@ -13,5 +24,15 @@ $(function () {
 		var lastItem = carouselList.find('li:last');
 		lastItem.after(firstItem);
 		carouselList.css({marginLeft: 0});
+		
 	}
+	
+	function moveLastSlide() {
+		var firstItem = carouselList.find('li:first');
+		var lastItem = carouselList.find('li:last');
+		firstItem.before(lastItem);
+		carouselList.css({marginLeft: 0});
+	}
+	
+	
 });
